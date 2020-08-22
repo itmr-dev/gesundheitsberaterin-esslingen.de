1edd411f04ea47f868e77a52c23c95af8fa562c9
craftycram <github@craftycram.net>
2020-08-22 18:50:10 +0200

    fixed mobile styling

--- src/App.vue
+++ src/App.vue
@@ -2,196 +2,208 @@
   <div id="app">
     <link href="https://fonts.googleapis.com/css2?family=Alata&display=swap" rel="stylesheet">
 
-    <b-jumbotron header="Cornelia Straub" lead="Gesundheitsberaterin GGB">
-      <!--<b-nav pills>
+
+    <!--<b-nav pills>
     <b-nav-item active>Startseite</b-nav-item>
     <b-nav-item>Aktuell</b-nav-item>
     <b-nav-item>Rezepte</b-nav-item>
   </b-nav>-->
+    <div>
+      <b-container>
+        <b-row>
+          <h1>Cornelia Straub</h1>
+        </b-row>
+        <b-row>
+          <h2 style="margin-top:0px; margin-bottom: 20px;">Gesundheitsberaterin GGB</h2>
+        </b-row>
+        <b-row>
+          <p style="margin-left: 0%; font-size: 1.5rem;">Gesünder essen</p>
+        </b-row>
+        <b-row>
+          <p style="margin-left: 5%; font-size: 1.5rem;">Mehr bewegen</p>
+        </b-row>
+        <b-row>
+          <p style="margin-left: 10%; font-size: 1.5rem;">Zur Ruhe kommen</p>
+        </b-row>
 
-      <br>
+        <b-row>
+          <h2>Aktuell</h2>
+        </b-row>
+        <b-row>
+          <h3>Vortag: Gesund durch vollwertige Ernährung</h3>
+        </b-row>
+        <b-row>
+          <p class="text">
+            Alle Krankheiten haben eine Ursache. Aber nur wenige Menschen wissen, dass ein großer Teil der Erkrankungen
+            durch falsche Ernährung entsteht.
+          </p>
+        </b-row>
+        <b-row>
+          <p class="text">
+            Vortrag am Mittwoch, den 28.10.2020 um 19:30 h bis ca. 20:45 h.
+          </p>
+        </b-row>
+        <b-row>
+          <p class="text">
+            Mit der bitte um eine verbindliche Anmeldung unter:
+            <br>
+            <a href="mailto:info@gesundheitsberaterin-esslingen.de">info@gesundheitsberaterin-esslingen.de</a>
+            <br>
+            Begrenzte Teilnehmerzahl
+          </p>
+        </b-row>
+        <b-row>
+          <p class="text">
+            Kosten: € 8,--
+          </p>
+        </b-row>
+        <b-row>
+          <p class="text">
+            Veranstaltungsort:
+            Familienzentrum Esslingen
+            Bachstr. 6
+            73730 Esslingen
+          </p>
+        </b-row>
 
-      <div>
-        <b-container fluid>
-          <b-row>
-            <p style="margin-left: 0%; font-size: 1.5rem;">Gesünder essen</p>
-          </b-row>
-          <b-row>
-            <p style="margin-left: 5%; font-size: 1.5rem;">Mehr bewegen</p>
-          </b-row>
-          <b-row>
-            <p style="margin-left: 10%; font-size: 1.5rem;">Zur Ruhe kommen</p>
-          </b-row>
-
-          <b-row>
-            <h2>Aktuell</h2>
-          </b-row>
-          <b-row>
-            <h3>Vortag: Gesund durch vollwertige Ernährung</h3>
-          </b-row>
-            <b-row>
-              <p class="text">
-                Alle Krankheiten haben eine Ursache. Aber nur wenige Menschen wissen, dass ein großer Teil der Erkrankungen durch falsche Ernährung entsteht.
-              </p>
-            </b-row>
-            <b-row>
-              <p class="text">
-                Vortrag am Mittwoch, den 28.10.2020 um 19:30 h bis ca. 20:45 h.
-              </p>
-            </b-row>
-            <b-row>
-              <p class="text">
-                Mit der bitte um eine verbindliche Anmeldung unter:
-                <br>
-                <a href="mailto:info@gesundheitsberaterin-esslingen.de">info@gesundheitsberaterin-esslingen.de</a>
-                <br>
-                Begrenzte Teilnehmerzahl
-              </p>
-            </b-row>
-            <b-row>
-              <p class="text">
-                Kosten: € 8,--
-              </p>
-            </b-row>
-            <b-row>
-              <p class="text">
-                Veranstaltungsort:
-                Familienzentrum Esslingen
-                Bachstr. 6
-                73730 Esslingen
-              </p>
-            </b-row>
-
-          <b-row>
-            <h2>Über mich</h2>
-          </b-row>
-          <b-row>
-            <b-col>
-              <ul class="fa-ul">
-                <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>1966 in Esslingen geboren</li>
-                <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>verheiratet und 2 erwachsene Kinder</li>
-                <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>gelernte Bürokauffrau</li>
-                <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>Übungsleiterin Aerobic</li>
-                <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>Trainerin B Prävention Erwachsene</li>
-                <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>Rückentrainerin</li>
-                <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>Pilatestrainerin</li>
-                <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>Beckenbodentrainerin</li>
-                <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>Achtsamkeitstrainerin</li>
-              </ul>
-              <p class="text" style="font-weight: 800;">ärztlich geprüfte Gesundheitsberaterin GGB</p>
-            </b-col>
-            <b-col>
-              <b-img src="./assets/imgs/conny.jpg" fluid alt="Cornelia Straub" style="padding-top: 10px; max-height: 30ch; width: auto;" />
-            </b-col>
-          </b-row>
-
-          <b-row>
-            <b-col>
-              <h2>Ich biete an</h2>
-              <ul class="fa-ul">
-                <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>Gesundheitsberatung</li>
-                <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>Ernährungseinzelberatung</li>
-                <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>Einkaufsbegleitung</li>
-                <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>Vorträge</li>
-                <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>Meditation</li>
-                <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>Rückentraining</li>
-                <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>Pilates</li>
-              </ul>
-            </b-col>
-            <b-col>
-              <h2>Interesse</h2>
-              <p class="text">Wir sind eine Körper-Geist-Seele Einheit.
-                Mir ist es ein Anliegen, interessierten Menschen meine Erfahrungen und das Wissen über die Zusammenhänge
-                der eigenen Lebensweise und der Krankheit bzw. Gesundheit weiterzugeben.
-                Und das neu Erlernte auszuprobieren.
-              </p>
-            </b-col>
-          </b-row>
-          <b-row>
-            <b-col>
-              <b-img src="./assets/imgs/salat.jpg" fluid alt="Salat" style="padding-top: 10px" />
-            </b-col>
-            <b-col>
-              <b-img src="./assets/imgs/brot.jpg" fluid alt="Brot" style="padding-top: 10px" />
-            </b-col>
-            <b-col>
-              <b-img src="./assets/imgs/hauptgericht.jpg" fluid alt="Hauptgericht"
-                style="padding-top: 10px;" />
-            </b-col>
-            <b-col>
-              <b-img src="./assets/imgs/nachtisch.jpg" fluid alt="Nachtisch" style="padding-top: 10px" />
-            </b-col>
-          </b-row>
-
-          <b-row>
-            <h2>Ernährungsbedingte Zivilisationskrankheiten nach Dr. Max Otto Bruker</h2>
-          </b-row>
-          <b-row>
+        <b-row>
+          <h2>Über mich</h2>
+        </b-row>
+        <b-row>
+          <b-col>
             <ul class="fa-ul">
-              <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>Gebissverfall, Zahnkaries, Parodontose und Zahnfehlstellungen</li>
-              <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>Erkrankungen des Bewegungsapparates, so genannte rheumatische Erkrankungen</li>
-              <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>Alle Stoffwechselkrankheiten wie Übergewicht und Zuckerkrankheit</li>
-              <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>Die meisten Erkrankungen der Verdauungsorgane wie Stuhlverstopfung</li>
-              <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>Gefäßerkrankungen wie Arteriosklerose, Herzinfarkt, Schlaganfall und Thrombosen</li>
-              <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>Mangelnde Infektabwehr</li>
-              <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>Die meisten der so genannten Allergien</li>
-              <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>Manche organische Erkrankungen des Nervensystems, z.B. MS</li>
-              <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>Auch an der Entstehung des Krebses ist die Fehlernährung in erheblichem Maße beteiligt</li>
+              <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>1966 in Esslingen geboren</li>
+              <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>verheiratet und 2 erwachsene Kinder</li>
+              <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>gelernte Bürokauffrau</li>
+              <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>Übungsleiterin Aerobic</li>
+              <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>Trainerin B Prävention Erwachsene</li>
+              <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>Rückentrainerin</li>
+              <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>Pilatestrainerin</li>
+              <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>Beckenbodentrainerin</li>
+              <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>Achtsamkeitstrainerin</li>
             </ul>
-          </b-row>
+            <p class="text" style="font-weight: 800;">ärztlich geprüfte Gesundheitsberaterin GGB</p>
+          </b-col>
+          <b-col>
+            <b-img src="./assets/imgs/conny.jpg" fluid alt="Cornelia Straub"
+              style="padding-top: 10px; max-height: 30ch; width: auto;" />
+          </b-col>
+        </b-row>
 
-          <b-row>
-            <h2>Kontakt</h2>
-          </b-row>
-          <b-row class="text">Mail: <a href="mailto:info@gesundheitsberaterin-esslingen.de"> info@gesundheitsberaterin-esslingen.de</a></b-row>
-          <b-row class="text">Telefon: <a href="tel:+49711 367728"> 0711 367728</a></b-row>
-          <b-row class="text">Weitere Informationen bei der Gesellschaft für Gesundheitsberatung GGB e.V. in 56112 Lahnstein</b-row>
+        <b-row>
+          <b-col>
+            <h2>Ich biete an</h2>
+            <ul class="fa-ul">
+              <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>Gesundheitsberatung</li>
+              <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>Ernährungseinzelberatung</li>
+              <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>Einkaufsbegleitung</li>
+              <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>Vorträge</li>
+              <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>Meditation</li>
+              <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>Rückentraining</li>
+              <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>Pilates</li>
+            </ul>
+          </b-col>
+          <b-col>
+            <h2>Interesse</h2>
+            <p class="text">Wir sind eine Körper-Geist-Seele Einheit.
+              Mir ist es ein Anliegen, interessierten Menschen meine Erfahrungen und das Wissen über die Zusammenhänge
+              der eigenen Lebensweise und der Krankheit bzw. Gesundheit weiterzugeben.
+              Und das neu Erlernte auszuprobieren.
+            </p>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col>
+            <b-img src="./assets/imgs/salat.jpg" fluid alt="Salat" style="padding-top: 10px" />
+          </b-col>
+          <b-col>
+            <b-img src="./assets/imgs/brot.jpg" fluid alt="Brot" style="padding-top: 10px" />
+          </b-col>
+          <b-col>
+            <b-img src="./assets/imgs/hauptgericht.jpg" fluid alt="Hauptgericht" style="padding-top: 10px;" />
+          </b-col>
+          <b-col>
+            <b-img src="./assets/imgs/nachtisch.jpg" fluid alt="Nachtisch" style="padding-top: 10px" />
+          </b-col>
+        </b-row>
 
-          <b-row>
-            <h2>Rezept Frischkornbrei nach Prof. Kollath</h2>
-          </b-row>
-          <b-row style="padding-top:10px;">
-            <b-col>
-              <b-img src="./assets/imgs/frischkornbrei.jpg" fluid alt="Rezept Frischkornbrei nach Prof. Kollath"
-                style="padding-top: 10px">
-              </b-img>
-            </b-col>
-            <b-col cols="8">
-              <p class="text">
-                Es wird aus Roggen oder Weizen oder einer beliebigen Getreideart oder Getreidemischung hergestellt.
-              </p>
-              <p class="text">
-                Dazu werden 3 Esslöffel (etwa 50 g) Getreide in einer Getreidemühle, in einen Mixapparat oder einer
-                Kaffeemühle grob geschrotet.
-              </p>
-              <p class="text">
-                Das Mahlen muss jedes Mal frisch vor der Zubereitung vorgenommen werden.
-              </p>
-              <p class="text">
-                Nicht auf Vorrat mahlen! Dabei spielt es keine Rolle, ob die Getreidemühle mit Mahlsteinen oder einem
-                Stahlwerk arbeitet.
-              </p>
-              <p class="text">
-                Das gemahlene Getreide wird mit ungekochtem, kaltem Leitungswasser zu einem Brei gerührt und
-                5-12 Stunden stehen gelassen. Die Wassermenge ist so berechnet, dass nach Quellung nichts weggegossen zu
-                werden braucht. Danach wird der Brei tischfertig gemacht durch Zusatz von frischem Obst (je nach
-                Jahreszeit),
-                Zitronensaft, 1-2 Esslöffel Sahne und geriebenen Nüssen.
-              </p>
-              <p class="text">
-                Solange verfügbar, sollte man immer einen Apfel hinein reiben und sogleich untermischen, bevor er braun
-                wird.
-                Der geriebene Apfel macht den Frischkornbrei luftig und wohlschmeckend.
-                Es ist ohne Belang, zu welcher Tageszeit dieses Gericht genossen wird.
+        <b-row>
+          <h2>Ernährungsbedingte Zivilisationskrankheiten nach Dr. Max Otto Bruker</h2>
+        </b-row>
+        <b-row>
+          <ul class="fa-ul">
+            <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>Gebissverfall, Zahnkaries, Parodontose und
+              Zahnfehlstellungen</li>
+            <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>Erkrankungen des Bewegungsapparates, so
+              genannte rheumatische Erkrankungen</li>
+            <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>Alle Stoffwechselkrankheiten wie Übergewicht
+              und Zuckerkrankheit</li>
+            <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>Die meisten Erkrankungen der Verdauungsorgane
+              wie Stuhlverstopfung</li>
+            <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>Gefäßerkrankungen wie Arteriosklerose,
+              Herzinfarkt, Schlaganfall und Thrombosen</li>
+            <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>Mangelnde Infektabwehr</li>
+            <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>Die meisten der so genannten Allergien</li>
+            <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>Manche organische Erkrankungen des
+              Nervensystems, z.B. MS</li>
+            <li><span class="fa-li"><i class="fas fa-apple-alt"></i></span>Auch an der Entstehung des Krebses ist die
+              Fehlernährung in erheblichem Maße beteiligt</li>
+          </ul>
+        </b-row>
 
-              </p>
-            </b-col>
+        <b-row>
+          <h2>Kontakt</h2>
+        </b-row>
+        <b-row class="text">Mail: <a href="mailto:info@gesundheitsberaterin-esslingen.de">
+            info@gesundheitsberaterin-esslingen.de</a></b-row>
+        <b-row class="text">Telefon: <a href="tel:+49711 367728"> 0711 367728</a></b-row>
+        <b-row class="text">Weitere Informationen bei der Gesellschaft für Gesundheitsberatung GGB e.V. in 56112
+          Lahnstein</b-row>
 
-          </b-row>
-        </b-container>
+        <b-row>
+          <h2>Rezept Frischkornbrei nach Prof. Kollath</h2>
+        </b-row>
+        <b-row style="padding-top:10px;">
+          <b-col>
+            <b-img src="./assets/imgs/frischkornbrei.jpg" fluid alt="Rezept Frischkornbrei nach Prof. Kollath"
+              style="padding-top: 10px">
+            </b-img>
+          </b-col>
+          <b-col cols="8">
+            <p class="text">
+              Es wird aus Roggen oder Weizen oder einer beliebigen Getreideart oder Getreidemischung hergestellt.
+            </p>
+            <p class="text">
+              Dazu werden 3 Esslöffel (etwa 50 g) Getreide in einer Getreidemühle, in einen Mixapparat oder einer
+              Kaffeemühle grob geschrotet.
+            </p>
+            <p class="text">
+              Das Mahlen muss jedes Mal frisch vor der Zubereitung vorgenommen werden.
+            </p>
+            <p class="text">
+              Nicht auf Vorrat mahlen! Dabei spielt es keine Rolle, ob die Getreidemühle mit Mahlsteinen oder einem
+              Stahlwerk arbeitet.
+            </p>
+            <p class="text">
+              Das gemahlene Getreide wird mit ungekochtem, kaltem Leitungswasser zu einem Brei gerührt und
+              5-12 Stunden stehen gelassen. Die Wassermenge ist so berechnet, dass nach Quellung nichts weggegossen zu
+              werden braucht. Danach wird der Brei tischfertig gemacht durch Zusatz von frischem Obst (je nach
+              Jahreszeit),
+              Zitronensaft, 1-2 Esslöffel Sahne und geriebenen Nüssen.
+            </p>
+            <p class="text">
+              Solange verfügbar, sollte man immer einen Apfel hinein reiben und sogleich untermischen, bevor er braun
+              wird.
+              Der geriebene Apfel macht den Frischkornbrei luftig und wohlschmeckend.
+              Es ist ohne Belang, zu welcher Tageszeit dieses Gericht genossen wird.
 
-      </div>
-    </b-jumbotron>
+            </p>
+          </b-col>
+
+        </b-row>
+      </b-container>
+
+    </div>
   </div>
 </template>
 
@@ -208,7 +220,7 @@
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     color: #2c3e50;
-    margin: 60px;
+    margin: 20px;
     margin-top: 60px;
   }
 
